d70211ac2df87c7796c27c127e911703b7cca80f
M00St4r <bjoern.martens@posteo.de>
2024-06-19 22:42:19 +0200

    add Stars to the Scene

--- Portfoliopage/3D_Test_Proj/main.js
+++ Portfoliopage/3D_Test_Proj/main.js
@@ -29,10 +29,25 @@ const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(3,3,3);
 pointLight.power = 10000;
 
-scene.add(pointLight);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(lightHelper);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x,y,z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x,y,z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
 
 function animate(){
   requestAnimationFrame(animate);
