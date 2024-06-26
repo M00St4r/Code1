c1686a979762be7c20ecf3f623405898db8229ed
M00St4r <bjoern.martens@posteo.de>
2024-06-26 13:28:44 +0200

    add afafbh hb af

---
+++ Files/blackmailer2/main.js
@@ -0,0 +1,28 @@
+console.log("Start");
+var chosenCharacter;
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
+    var mail = document.querySelector("div#mail");
+    mail.addEventListener("click", placeLetter);
+    document.addEventListener("keydown", choseCharacter);
+}
+function placeLetter(_event) {
+    var mouseX = _event.offsetX;
+    var mouseY = _event.offsetY;
+    var letter = document.createElement("span");
+    var mail = document.querySelector("div#mail");
+    mail.appendChild(letter);
+    letter.textContent = chosenCharacter;
+    letter.style.aspectRatio = "1/1";
+    letter.style.left = mouseX.toString() + "px";
+    letter.style.top = mouseY.toString() + "px";
+    letter.addEventListener("click", deleteLetter);
+}
+function choseCharacter(_event) {
+    chosenCharacter = _event.key;
+}
+function deleteLetter(_event) {
+    var target = _event.target;
+    var targetParent = target.parentNode;
+    targetParent.removeChild(target);
+}
--- Files/blackmailer2/main.ts
+++ Files/blackmailer2/main.ts
@@ -1,1 +1,36 @@
+console.log("Start");
+let chosenCharacter: string;
 
+window.addEventListener("load", handleLoad);
+
+function handleLoad(_event: Event): void {
+    let mail: HTMLElement = <HTMLElement>document.querySelector("div#mail");
+    mail.addEventListener("click", placeLetter);
+    document.addEventListener("keydown", choseCharacter);
+}
+
+function placeLetter(_event: MouseEvent): void {
+    let mouseX: number = _event.offsetX;
+    let mouseY: number = _event.offsetY;
+
+    let letter: HTMLSpanElement = document.createElement("span");
+    let mail: HTMLElement = <HTMLElement>document.querySelector("div#mail");
+    mail.appendChild(letter);
+
+    letter.textContent = chosenCharacter;
+    letter.style.aspectRatio = "1/1";
+    letter.style.left = mouseX.toString() + "px";
+    letter.style.top = mouseY.toString() + "px";
+
+    letter.addEventListener("click", deleteLetter);
+}
+
+function choseCharacter(_event: KeyboardEvent): void {
+    chosenCharacter = _event.key;
+}
+
+function deleteLetter(_event: MouseEvent): void {
+    let target: Node = <Node>_event.target;
+    let targetParent: Node = <Node>target.parentNode;
+    targetParent.removeChild(target);
+}
