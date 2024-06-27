1432658c4485219f6275a331626af079e9c197be
M00St4r <bjoern.martens@posteo.de>
2024-06-27 15:16:55 +0200

    update canvas folder

--- Files/08canvas/main.js
+++ Files/08canvas/main.js
@@ -1,15 +1,34 @@
-"use strict";
-document.addEventListener("load", handleLoad);
-function handleLoad(ev) {
-    const canvas = document.getElementsByTagName("canvas")[0];
-    const ctx = canvas.getContext("2d");
-    ctx.rect(10, 10, 100, 100);
-    //ctx.fill();
-    ctx.strokeStyle = "blue";
-    ctx.lineWidth = 5;
-    ctx.stroke();
-    ctx.fillStyle = "rgb(255, 0, 255)";
-    let path = new Path2D();
-    path.rect(200, 200, 50, 50);
-    ctx.fill(path);
-}
+//document.addEventListener("load", handleLoad)
+var canvas = document.getElementsByTagName("canvas")[0];
+// canvas.style.top = "0px";
+// canvas.style.left = "0px";
+// canvas.style.height = "600px";
+// canvas.style.width = "600px";
+document.body.appendChild(canvas);
+var ctx = canvas.getContext("2d");
+ctx.rect(10, 10, 100, 100);
+//ctx.fill();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.stroke();
+// Draw Square
+ctx.fillStyle = "rgb(255, 0, 255)";
+var squawe = new Path2D();
+squawe.rect(10, 300, 50, 50);
+ctx.fill(squawe);
+// Draw Arc
+var arc = new Path2D();
+arc.arc(300, 105, 100, 0, 180, true);
+ctx.stroke(arc);
+var posX = 500;
+var posY = 0;
+var w = 100;
+var h = 100;
+// gradient
+var gradient = ctx.createLinearGradient(0, posY, 0, h);
+gradient.addColorStop(0, "red");
+gradient.addColorStop(1, "black");
+var UwU = new Path2D();
+UwU.roundRect(posX, posY, w, h, 5);
+ctx.fillStyle = gradient;
+ctx.fill(UwU);
--- Files/08canvas/main.ts
+++ Files/08canvas/main.ts
@@ -1,18 +1,43 @@
-document.addEventListener("load", handleLoad)
+//document.addEventListener("load", handleLoad)
 
-function handleLoad(this: Document, ev: Event) {
-    const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
-    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
+// canvas.style.top = "0px";
+// canvas.style.left = "0px";
+// canvas.style.height = "600px";
+// canvas.style.width = "600px";
+document.body.appendChild(canvas);
+const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
-    ctx.rect(10, 10, 100, 100);
-    //ctx.fill();
-    ctx.strokeStyle = "blue";
-    ctx.lineWidth = 5;
-    ctx.stroke();
+ctx.rect(10, 10, 100, 100);
+//ctx.fill();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.stroke();
 
-    ctx.fillStyle = "rgb(255, 0, 255)";
-    let path: Path2D = new Path2D();
-    path.rect(200, 200, 50, 50);
-    ctx.fill(path);
+// Draw Square
+ctx.fillStyle = "rgb(255, 0, 255)";
+let squawe: Path2D = new Path2D();
+squawe.rect(10, 300, 50, 50);
+ctx.fill(squawe);
 
-}
+// Draw Arc
+let arc: Path2D = new Path2D();
+arc.arc(300, 105, 100, 0, 180, true);
+ctx.stroke(arc);
+
+let posX = 500;
+let posY = 0;
+
+let w = 100;
+let h = 100;
+
+// gradient
+let gradient = ctx.createLinearGradient(0, posY, 0, h);
+gradient.addColorStop(0, "red");
+gradient.addColorStop(1, "black");
+
+let UwU: Path2D = new Path2D();
+UwU.roundRect(posX, posY, w, h, 5);
+
+ctx.fillStyle = gradient;
+ctx.fill(UwU);
