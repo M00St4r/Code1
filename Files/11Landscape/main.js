79cbe0357f7301b25495b3ea3dac15dd515e8a88
M00St4r <bjoern.martens@posteo.de>
2024-07-01 12:55:02 +0200

    wow error >:C

---
+++ Files/11Landscape/main.js
@@ -0,0 +1,12 @@
+var Tree1 = {
+    transform: {
+        posX: 0,
+        posY: 0,
+        width: 10,
+        height: 50,
+        scale: 1,
+    },
+    type: "pine",
+    color: "green",
+    hasLeaves: true,
+};
--- Files/11Landscape/main.ts
+++ Files/11Landscape/main.ts
@@ -3,15 +3,30 @@ interface Transform {
     posX: number,
     width: number,
     height: number,
-    size: number,
+    scale: number,
 }
 
 interface Tree {
     transform: Transform,
     type: string,
+    color: string,
+    hasLeaves: boolean,
 }
 
 interface Cloud {
     transform: Transform,
     type: string,
+}
+
+let Tree1: Tree = {
+    transform: {
+        posX: 0,
+        posY: 0,
+        width: 10,
+        height: 50,
+        scale: 1,
+    },
+    type: "pine",
+    color: "green",
+    hasLeaves: true,
 }
